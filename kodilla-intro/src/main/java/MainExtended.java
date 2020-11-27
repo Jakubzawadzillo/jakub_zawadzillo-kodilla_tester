4566266023501c18d3fb46013d1b7fbc21bee693
Jakub Zawadzillo <qbazaw@gmail.com>
2020-11-27 18:51:08 +0100

    Second Code

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String instrumentName= "Guitar";
+        String instrumentType= "Bass";
+        int strings= 4;
+        double practiceYears= 2.55;
+        int colourNmMax= 410;
+        double piNo= 3.14;
+
+        System.out.println(instrumentName);
+        System.out.println(instrumentType);
+        System.out.println(strings);
+        System.out.println(practiceYears);
+        System.out.println(colourNmMax);
+        System.out.println(piNo);
+
+    }
+}
---
+++ kodilla-intro/src/main/java/SecondCode.java
@@ -0,0 +1,18 @@
+public class SecondCode {
+    public static void main(String[] args) {
+        String myName= "Maciej";
+        int myAge= 22;
+        double height= 1.78;
+        char carClass= 'A';
+        boolean isGoodProgrammer= true;
+
+        String Hello= "Hello from FirstClass";
+
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+        System.out.println(Hello);
+    }
+}
