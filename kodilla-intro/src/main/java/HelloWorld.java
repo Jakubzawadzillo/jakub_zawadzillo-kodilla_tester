d94f1bc2f33d66e2437d3c0ba5273d2a693487ce
Jakub Zawadzillo <qbazaw@gmail.com>
2020-11-27 18:27:42 +0100

    Hello World Program

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World!"); // [3]
+        System.out.println("Jakub");
+        System.out.println("28");
+        System.out.println("Z");
+    }
+}
